7215c3be2df37cbe90d6c1e0c0dec2af27593eef
JulioDinizN <juliocesardiniznogueira@gmail.com>
2021-11-21 20:03:44 -0300

    Stripe and fauna almost done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SingInButton() {
-    const isUserLoggedIn = true
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => signOut()}
         >
             <FaGithub color='#04d361'/>
-            Diego Fernandes
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon}/>
         </button>
     ) : (
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417'/>
             Sing in with GitHub
