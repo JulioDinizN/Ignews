b47352d79d5d9e6d7252297968f348eb4dacbe84
JulioDinizN <juliocesardiniznogueira@gmail.com>
2021-11-22 21:58:28 -0300

    Project Completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { SingInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
+import Image from 'next/image'
 import styles from './styles.module.scss'
 
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
+                {/* <Image width='110px' height='110px' src="/images/logo.svg" alt="ig.news"/> */}
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a className={styles.active}>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts'>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SingInButton />
             </div>
